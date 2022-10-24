44c722b4190405351aa20beffa7883f689ae490a
Vinod John <vinod.infant.dass.john.rozario@luminorgroup.com>
2022-10-24 20:56:55 +0300

    Encapsulation and inheritance done

---
+++ src/main/java/org/sda/model/Person.java
@@ -0,0 +1,108 @@
+package org.sda.model;
+
+import java.util.Random;
+
+/**
+ * Person model
+ * Example of Encapsulation
+ *
+ * @author Vinod John
+ */
+public class Person {
+    // Fields
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String address;
+
+    // All arguments constructor
+    public Person(Long id, String firstName, String lastName, String email, String phoneNumber, String address) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    // Parameterized constructor
+    public Person(Long id, String address) {
+        this.id = id;
+        this.address = address;
+    }
+
+    //Java will create a constructor automatically by default. it is called as default constructor.
+
+    //No-Arg constructor
+    public Person() {
+        this.id = new Random().nextLong();
+    }
+
+
+    //Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        if(!firstName.isEmpty()) {
+            this.firstName = firstName;
+        }
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+}
