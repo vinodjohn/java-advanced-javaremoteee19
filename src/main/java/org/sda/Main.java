e4d7443e4090798af4838bb3b4d74ebcac0aed63
Vinod John <vinod.infant.dass.john.rozario@luminorgroup.com>
2022-11-02 20:23:29 +0200

    List examples added.

--- src/main/java/org/sda/Main.java
+++ src/main/java/org/sda/Main.java
@@ -3,6 +3,9 @@ package org.sda;
 import org.sda.generics.*;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -32,5 +35,45 @@ public class Main {
         Snack snack1 = new Snack(5);
         Snack snack2 = new Snack(3);
         System.out.println("Snack 1 is better than Snack 2: " + snack1.isBetter(snack2));
+
+        //LIST
+        List<String> animalList = new ArrayList<>();
+        animalList.add("Lion"); // 0
+        animalList.add("Tiger"); // 1
+        animalList.add("Dog"); // 2
+        animalList.add("Cat");
+        animalList.add("Bear");
+        animalList.add("Panda");
+
+        System.out.println(animalList.get(1)); // prints tiger
+
+        animalList.remove(1); // tiger will be deleted
+        animalList.remove("Dog");
+
+        for (String animal : animalList) {
+            System.out.println(animal);
+        }
+
+        Iterator<String> stringIterator = animalList.listIterator();
+
+        while(stringIterator.hasNext()) {
+            System.out.println(stringIterator.next() + " ,");
+        }
+
+        // List of items to be removed
+        List<String> removeAnimalList = new ArrayList<>();
+        removeAnimalList.add("Cat");
+        removeAnimalList.add("Panda");
+
+        animalList.removeAll(removeAnimalList);
+
+        //Final Removed list
+        for (String animal : animalList) {
+            System.out.println(animal);
+        }
     }
+
+
+
+
 }
