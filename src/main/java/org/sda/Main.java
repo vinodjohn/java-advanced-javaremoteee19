a53de8517f113aa10f724bb6dbf11b80e9f1757b
Vinod John <vinod.infant.dass.john.rozario@luminorgroup.com>
2022-11-03 19:27:45 +0200

    Map examples added

--- src/main/java/org/sda/Main.java
+++ src/main/java/org/sda/Main.java
@@ -3,9 +3,7 @@ package org.sda;
 import org.sda.generics.*;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -71,6 +69,42 @@ public class Main {
         for (String animal : animalList) {
             System.out.println(animal);
         }
+
+
+        //SET
+        Set<String> countrySet = new HashSet<>(); // Non-sorted, randomly stored
+        countrySet.add("Eesti");    // 525632
+        countrySet.add("Saksmaa");  // 152635
+        countrySet.add("Poola");
+        countrySet.add("Rootsi");
+        // countrySet.add("Eesti"); -> Duplicates not allowed!
+
+        for (String country: countrySet) {
+            System.out.println(country);
+        }
+
+        System.out.println("Before sorting: " + countrySet);
+        TreeSet<String> countryTreeSet = new TreeSet<>(countrySet); // Stored as Sorted
+        System.out.println("After sorting: " + countryTreeSet);
+
+
+        //MAP
+        Map<String,String> fullName = new HashMap<>(); // Not stored as sorted
+        fullName.put("Vinod", "John");
+        fullName.put("Martti", "Triksberg");
+        fullName.put("Marko", "Piir");
+        System.out.println(fullName);
+
+        System.out.println(fullName.get("Vinod"));
+        System.out.println(fullName.remove("Marko"));
+        System.out.println(fullName);
+
+
+        Map<String, Integer> ageMap = new HashMap<>();
+
+        Map<String, List<String>> friendsMap = new HashMap<>();
+
+        Map<String, Map<String, String>> detailsMap = new HashMap<>();
     }
 
 
